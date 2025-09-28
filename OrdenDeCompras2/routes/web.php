bc09f8f2f79fa920d4ac2bb66f25cdc90c2e2ae5
Zerok531-wq <booh6121@gmail.com>
2025-09-28 17:20:25 -0600

    boo

---
+++ OrdenDeCompras2/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\nav;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/',    [nav::class, 'index'])      ->name('index');
+Route::get('/productos',[nav::class, 'productos'])  ->name('productos');
+Route::get('/pedidos',  [nav::class, 'pedidos'])    ->name('pedidos');
+Route::get('/perfil',   [nav::class, 'perfil'])     ->name('perfil');
